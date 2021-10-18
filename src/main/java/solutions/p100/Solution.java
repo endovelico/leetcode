4984530db82941d7e3dda79691d9bc8d8c395a00
Flavio <senabrasousa@tutanota.com>
2021-10-19 00:01:38 +0200

    Heres solution for number 100

--- src/main/java/solutions/p100/Solution.java
+++ src/main/java/solutions/p100/Solution.java
@@ -3,9 +3,13 @@ package solutions.p100;
 import common.TreeNode;
 
 public class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+    public boolean isSameTree(TreeNode first_tree, TreeNode second_tree) {
 
+        if( first_tree == null && second_tree == null) return true;
+        if( first_tree == null || second_tree == null) return false;
+        if( first_tree.val != second_tree.val ) return false;
 
-        return true;
+        return isSameTree( first_tree.left, second_tree.left )
+                && isSameTree( first_tree.right, second_tree.right );
     }
 }

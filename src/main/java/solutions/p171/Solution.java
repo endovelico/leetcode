d059141fe58bf9be5544d053ffe4edecf6c40956
Flavio <senabrasousa@gmail.com>
2024-03-09 18:25:26 +0100

    class addition

---
+++ src/main/java/solutions/p171/Solution.java
@@ -0,0 +1,14 @@
+package solutions.p171;
+
+import java.util.HashMap;
+
+public class Solution {
+
+    HashMap<Integer, String> map = new HashMap<>();
+
+    public int titleToNumber(String columnTitle) {
+
+
+        return 0;
+    }
+}

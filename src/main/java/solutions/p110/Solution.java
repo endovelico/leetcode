5f08f2887e3710c738230320d986599dc0754a96
Flavio <senabrasousa@gmail.com>
2025-05-27 22:10:41 +0200

    scaffolding for p110

---
+++ src/main/java/solutions/p110/Solution.java
@@ -0,0 +1,26 @@
+package solutions.p110;
+
+import common.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+
+
+        return true;
+    }
+}

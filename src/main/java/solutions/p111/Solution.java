645e391c114f21376ed698a2e86509afaa71215a
Flavio <senabrasousa@gmail.com>
2025-06-13 17:20:32 +0100

    solution

--- src/main/java/solutions/p111/Solution.java
+++ src/main/java/solutions/p111/Solution.java
@@ -5,18 +5,19 @@ import common.TreeNode;
 public class Solution {
 
     public int minDepth(TreeNode root) {
-        int current_min_depth = 0;
-        if (root == null) return current_min_depth;
-
-        return depth_first_search(root, current_min_depth);
+        if (root == null) return 0;
+        return depth_first_search(root);
     }
 
-    private int depth_first_search(TreeNode root, int current_min_depth) {
+    private int depth_first_search(TreeNode node) {
+        if (node == null) return Integer.MAX_VALUE;
 
-        if (root == null) return Integer.MAX_VALUE;
+        // If it's a leaf node
+        if (node.left == null && node.right == null) return 1;
 
-        int new_current_min_depth = current_min_depth + 1;
+        int leftDepth = depth_first_search(node.left);
+        int rightDepth = depth_first_search(node.right);
 
-        return Math.min(depth_first_search(root.left, new_current_min_depth), depth_first_search(root.right, new_current_min_depth));
+        return 1 + Math.min(leftDepth, rightDepth);
     }
 }

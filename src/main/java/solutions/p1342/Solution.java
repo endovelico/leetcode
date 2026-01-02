76583b4338e7b0526142426b11ae3c9cbda57a33
Flavio <senabrasousa@gmail.com>
2026-01-02 21:02:07 +0100

    steps

---
+++ src/main/java/solutions/p1342/Solution.java
@@ -0,0 +1,23 @@
+package solutions.p1342;
+
+public class Solution {
+
+    public int numberOfSteps(int num) {
+        int steps = 0;
+
+        while (num > 0) {
+
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num = num - 1;
+            }
+
+            steps++;
+
+        }
+
+        return steps;
+    }
+
+}

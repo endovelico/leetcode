c7c7319dd6bd4bce1dec9fdf86a266a005664095
Flavio <senabrasousa@gmail.com>
2026-09-21 22:53:39 +0200

    linked list medium diff

--- src/main/java/solutions/p1290/Solution.java
+++ src/main/java/solutions/p1290/Solution.java
@@ -5,7 +5,13 @@ import common.ListNode;
 class Solution {
     public int getDecimalValue(ListNode head) {
 
+        int result = 0;
 
-        return 0;
+        while(head != null) {
+            result = result * 2 + head.val;
+            head = head.next;
+        }
+
+        return result;
     }
 }

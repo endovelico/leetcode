fe8822bf5a6f3ae82c9ca9a2372ba704d49141fb
Flavio <senabrasousa@gmail.com>
2026-09-21 22:12:10 +0200

    linked list medium diff

--- src/main/java/solutions/p2181/Solution.java
+++ src/main/java/solutions/p2181/Solution.java
@@ -1,4 +1,32 @@
 package solutions.p2181;
 
-public class Solution {
+import common.ListNode;
+
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        int sum = 0;
+        ListNode curr = head.next; // skip the first 0
+
+        while (curr != null) {
+
+            if (curr.val == 0) {
+                // End of a group
+                tail.next = new ListNode(sum);
+                tail = tail.next;
+
+                sum = 0;
+            } else {
+                // Add value to current group
+                sum += curr.val;
+            }
+
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
 }

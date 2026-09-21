23f3677136e3e105094b20eb708774de2367eb5d
Flavio <senabrasousa@gmail.com>
2026-09-22 01:28:56 +0200

    linked list medium diff

---
+++ src/main/java/solutions/p3658/Solution.java
@@ -0,0 +1,7 @@
+package solutions.p3658;
+
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
--- src/main/java/solutions/p4010/Solution.java
+++ src/main/java/solutions/p4010/Solution.java
@@ -1,9 +1,37 @@
 package solutions.p4010;
 
 class Solution {
-    public long maxPairStrength(int[] nums) {
 
+    public long maxStrength(int[] nums) {
 
-        return 0L;
+        long maxStrength = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                long a = nums[i];
+                long b = nums[j];
+
+                long gcd = gcd(a, b);
+
+                long strength = (a * b) / (gcd * gcd);
+
+                maxStrength = Math.max(maxStrength, strength);
+            }
+        }
+
+        return maxStrength;
+    }
+
+    private long gcd(long a, long b) {
+
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
     }
 }

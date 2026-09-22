b7f6889e37080d94426dec5d1645e9c141ebdee9
Flavio <senabrasousa@gmail.com>
2026-09-22 02:07:47 +0200

    linked list medium diff

--- src/main/java/solutions/p1863/Solution.java
+++ src/main/java/solutions/p1863/Solution.java
@@ -2,7 +2,22 @@ package solutions.p1863;
 
 class Solution {
     public int subsetXORSum(int[] nums) {
+        return dfs(nums, 0, 0);
+    }
 
-        return 0;
+    private int dfs(int[] nums, int index, int xor) {
+
+        // We've decided whether to include every number
+        if (index == nums.length) {
+            return xor;
+        }
+
+        // Option 1: don't include nums[index]
+        int without = dfs(nums, index + 1, xor);
+
+        // Option 2: include nums[index]
+        int with = dfs(nums, index + 1, xor ^ nums[index]);
+
+        return without + with;
     }
 }
---
+++ src/main/java/solutions/p492/Solution.java
@@ -0,0 +1,16 @@
+package solutions.p492;
+
+class Solution {
+    public int[] constructRectangle(int area) {
+
+        int width = (int) Math.sqrt(area);
+
+        while (area % width != 0) {
+            width--;
+        }
+
+        int length = area / width;
+
+        return new int[]{length, width};
+    }
+}
